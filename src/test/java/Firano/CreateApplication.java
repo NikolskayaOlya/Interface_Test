ebb9a749e2ec188aad843cb1b94e611484ef2ab7
NikolskayaOlya <o.nikolskaya@isp.local>
2017-02-27 18:15:58 +0300

    n

--- src/test/java/Firano/CreateApplication.java
+++ src/test/java/Firano/CreateApplication.java
@@ -13,7 +13,7 @@ public class CreateApplication {
 
     @Test
     public void sendPostRequest () {
-        String [][] key = {{"surname", "name", "mobile"},{"Иван", "Иванович", "+7(911)901-99-83"}};
+        String [][] key = {{"surname", "name", "mobile"},{"Иван", "Иванович", "+7(911)901-99-89"}};
         int responseCode;
         String urlParameters = "?wm_id=4pMd&bank_id=1088&offer_id=28404&token=BqD7Xv2a9zcA9YXB";
         String query = "http://ff.duser046.test/api/rest/v1/applications.json";
