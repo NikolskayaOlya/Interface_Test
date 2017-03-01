b4c7dfc6621a0c9a343f44318fd1d98968e5df90
NikolskayaOlya <o.nikolskaya@isp.local>
2017-03-01 16:45:48 +0300

    set Webdriver

---
+++ src/main/java/settings/SetupDriver.java
@@ -0,0 +1,32 @@
+package settings;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by o.nikolskaya on 01.03.2017.
+ */
+public class SetupDriver {
+
+    public WebDriver setDriver(){
+
+        WebDriver driver = null;
+        String browser = System.getProperty("browser");
+
+        switch(browser){
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = new FirefoxDriver();
+                break;
+        }
+
+        return driver;
+    }
+
+}
--- src/test/java/publisher/CreateAccountTest.java
+++ src/test/java/publisher/CreateAccountTest.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import settings.SetupDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,27 +13,16 @@ import java.util.concurrent.TimeUnit;
  * Created by o.nikolskaya on 28.02.2017.
  */
 public class CreateAccountTest {
-    WebDriver driver;
+    WebDriver driver = null;
 
+    @BeforeTest
     public void setupWebDriver(){
-        System.setProperty("webdriver.chrome.driver", "C:/Users/o.nikolskaya/Desktop/programs/chromedriver.exe");
-        driver = new ChromeDriver();
-        // driver.get("http://beta3.cityads.com.br/");
+        driver = (new SetupDriver()).setDriver();
+    }
+
+    @Test(description = "open browser")
+    public void inn(){
         driver.get("https://beta.cityads.com.br/mng_new/payments/requests");
-        WebElement username = driver.findElement(By.id("auth_content_login_username"));
-        username.sendKeys("o.nikolskaya@cityads.com");//e.voronin+manager@cityads.com
-
-        WebElement password = driver.findElement(By.id("auth_content_login_password"));
-        password.sendKeys("Nikolskaya");  //123456789
-
-        WebElement sign_in = driver.findElement(By.className("button"));
-        sign_in.click();
-
-        driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-
-        WebElement login_link = driver.findElement(By.id("popup-login-link"));
-        login_link.click();
-      /*  driver.get("http://beta3.cityads.com.br/webmaster/office/dashboard");*/
     }
 
 
